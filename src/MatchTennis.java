0cc04bf4bb20239ddaaff227efffd948b5e37502
Zovopwoot <zovopwoot@gmail.com>
2019-04-24 15:49:59 +0200

    Ajout conditions de victoire

--- src/MatchTennis.java
+++ src/MatchTennis.java
@@ -1,6 +1,9 @@
 public class MatchTennis {
     String joueur1;
     String joueur2;
+    boolean isMatchInProgress =true;
+    boolean isP1playing = true;
+    boolean isP2playing = true;
     int scorePointJ1;
     int scorePointJ2;
     int scoreMancheJ1;
@@ -8,63 +11,78 @@ public class MatchTennis {
     boolean egalité = false;
     boolean avantageJ1 = false;
     boolean avantageJ2 = false;
-    int Manches = 1;
 
-    public class gainPointJ1(){//gestion du score Joueur 1
-        if (scorePointJ1 == 0 || scorePointJ1 < 3) {// augmentation d'un point
-            scorePointJ1++;
-        } else if (scorePointJ1 >= 3 && scorePointJ2 >= 3 && scorePointJ1 == scorePointJ2) { //conditions d'Egalité
-            egalité = true;
-            avantageJ1 = false;
-            avantageJ2 = false;
-        } else if (egalité && scorePointJ1 == scorePointJ2 + 1) {//condition d'Avantage
-            egalité = false;
-            avantageJ1 = true;
-            avantageJ2 =false;
-        } else if (avantageJ1) {
-            scoreMancheJ1++;
-            scorePointJ1 = 0;
-            scorePointJ2 = 0;
-            egalité = false;
-            avantageJ1 = false;
+
+    void gainPointJ1() {//gestion du score Joueur 1
+        if (isP1playing && isP2playing) {
+            if (scorePointJ1 == 0 || scorePointJ1 < 3) {// augmentation d'un point
+                scorePointJ1++;
+            } else if (scorePointJ1 >= 3 && scorePointJ2 >= 3 && scorePointJ1 == scorePointJ2) { //conditions d'Egalité
+                egalité = true;
+                avantageJ1 = false;
+                avantageJ2 = false;
+            } else if (egalité && scorePointJ1 == scorePointJ2 + 1) {//condition d'Avantage
+                egalité = false;
+                avantageJ1 = true;
+                avantageJ2 = false;
+            } else if (avantageJ1) { // gain de jeu
+                scoreMancheJ1++;
+                scorePointJ1 = 0;
+                scorePointJ2 = 0;
+                egalité = false;
+                avantageJ1 = false;
+                if (scoreMancheJ1 >= 6 && scoreMancheJ1 >= scoreMancheJ2 + 2) { //victoire J1
+                    abandonJ2();
+                    isMatchInProgress =false ;
+                }
+            }
         }
     }
 
-    public class gainPointJ2(){
-        if (scorePointJ2 == 0 || scorePointJ2 < 3) {// augmentation d'un point
-            scorePointJ2++;
-        } else if (scorePointJ2 >= 3 && scorePointJ1 >= 3 && scorePointJ1 == scorePointJ2) { //conditions d'Egalité
-            egalité = true;
-            avantageJ1 = false;
-            avantageJ2 =false;
-        } else if (egalité && scorePointJ2 == scorePointJ1 + 1) {//condition d'Avantage
-            egalité = false;
-            avantageJ1 = true;
-            avantageJ2 = false;
-        } else if (avantageJ2) {
-            scoreMancheJ2++;
-            scorePointJ1 = 0;
-            scorePointJ2 = 0;
-            egalité = false;
-            avantageJ1 = false;
-            avantageJ2 =false;
+
+    void gainPointJ2(){
+
+        if (isP1playing && isP2playing) {
+            if (scorePointJ2 == 0 || scorePointJ2 < 3) {// augmentation d'un point
+                scorePointJ2++;
+            } else if (scorePointJ2 >= 3 && scorePointJ1 >= 3 && scorePointJ1 == scorePointJ2) { //conditions d'Egalité
+                egalité = true;
+                avantageJ1 = false;
+                avantageJ2 = false;
+            } else if (egalité && scorePointJ2 == scorePointJ1 + 1) {//condition d'Avantage
+                egalité = false;
+                avantageJ1 = true;
+                avantageJ2 = false;
+            } else if (avantageJ2) {// gain de jeu
+                scoreMancheJ2++;
+                scorePointJ1 = 0;
+                scorePointJ2 = 0;
+                egalité = false;
+                avantageJ1 = false;
+                avantageJ2 = false;
+            }
+            if (scoreMancheJ2 >= 6 && scoreMancheJ1 >= scoreMancheJ1 + 2) { //victoire J2
+                abandonJ1();
+                isMatchInProgress = false;
+            }
         }
     }
 
-    public class abandonJ1()
 
-    {
+    void abandonJ1() {
+        isP1playing = false;
     }
 
-    public class abandonJ2()
 
-    {
+    void abandonJ2() {
+        isP2playing = false;
     }
 
-    public class toString()
-
-    {
+    String toString() {
+        String score
     }
 
 
 }
+
+
