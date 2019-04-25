1841960cc1f27acf46c485e27e613ceb82cab622
Zovopwoot <zovopwoot@gmail.com>
2019-04-25 11:41:17 +0200

    Encapsulation

--- src/MatchTennis.java
+++ src/MatchTennis.java
@@ -1,22 +1,27 @@
 public class MatchTennis {
-    String joueur1;
-    String joueur2;
-    boolean isMatchInProgress = true;
-    boolean isP1playing = true;
-    boolean isP2playing = true;
-    int pointJ1 = 0;
-    int pointJ2 = 0;
-    int nbreJeuJ1 = 0;
-    int nbreJeuJ2 = 0;
-    boolean egalité = false;
-    boolean avantageJ1 = false;
-    boolean avantageJ2 = false;
+    String joueur1 = new String();
+    String joueur2 = new String();
+    private boolean isMatchInProgress = true;
+    private boolean isP1playing = true;
+    private boolean isP2playing = true;
+    private int pointJ1 = 0;
+    private int pointJ2 = 0;
+    private int nbreJeuJ1 = 0;
+    private int nbreJeuJ2 = 0;
+    private boolean egalité = false;
+    private boolean avantageJ1 = false;
+    private boolean avantageJ2 = false;
 
-    void gainPointJ1() {//gestion du score Joueur 1
-        if (isP1playing && isP2playing) {
+    public void gainPointJ1() {//gestion du score Joueur 1
+        if (isMatchInProgress) {
             if (pointJ1 == 0 || pointJ1 < 3) {// augmentation d'un point
                 pointJ1++;
-            } else if (pointJ1 >= 3 && pointJ2 >= 3 && pointJ1 == pointJ2) { //conditions d'Egalité
+            }else if(pointJ1>=3 && pointJ2<3){ // gain de jeu
+                nbreJeuJ1++;
+                pointJ1 =0;
+                pointJ2 =0;
+            }
+            else if (pointJ2 >= 3 && pointJ1 == pointJ2) { //conditions d'Egalité
                 egalité = true;
                 avantageJ1 = false;
                 avantageJ2 = false;
@@ -38,9 +43,9 @@ public class MatchTennis {
         }
     }
 
-    void gainPointJ2() {
+    public void gainPointJ2() {
 
-        if (isP1playing && isP2playing) {
+        if (isMatchInProgress) {
             if (pointJ2 == 0 || pointJ2 < 3) {// augmentation d'un point
                 pointJ2++;
             } else if (pointJ2 >= 3 && pointJ1 >= 3 && pointJ1 == pointJ2) { //conditions d'Egalité
@@ -66,12 +71,14 @@ public class MatchTennis {
         }
     }
 
-    void abandonJ1() {
+    public void abandonJ1() {
         isP1playing = false;
+        isMatchInProgress =false;
     }
 
-    void abandonJ2() {
+    public void abandonJ2() {
         isP2playing = false;
+        isMatchInProgress = false;
     }
 
     public String toString() {
@@ -108,21 +115,21 @@ public class MatchTennis {
         if (isMatchInProgress) {
 
             if (avantageJ1) {
-                return nbreJeuJ1 + " : " + nbreJeuJ2 + " / " + scoreJeuJ1 + " : " + scoreJeuJ2 + " / Avantage : " + joueur1;
+                return nbreJeuJ1 + " - " + nbreJeuJ2 + "  " + scoreJeuJ1 + " - " + scoreJeuJ2 + " / Avantage : " + joueur1;
             } else if (avantageJ2) {
-                return nbreJeuJ1 + " : " + nbreJeuJ2 + " / " + scoreJeuJ1 + " : " + scoreJeuJ2 + " / Avantage : " + joueur2;
+                return nbreJeuJ1 + " - " + nbreJeuJ2 + "  " + scoreJeuJ1 + " - " + scoreJeuJ2 + " / Avantage : " + joueur2;
             } else {
-                return nbreJeuJ1 + " : " + nbreJeuJ2 + " / " + scoreJeuJ1 + " : " + scoreJeuJ2;
+                return nbreJeuJ1 + " - " + nbreJeuJ2 + " " + scoreJeuJ1 + " - " + scoreJeuJ2;
             }
 
         } else {
 
             if (!isP2playing) {
-                return nbreJeuJ1 + " : " + nbreJeuJ2 + "Abandon J2 - Victoire J1";
+                return nbreJeuJ1 + " - " + nbreJeuJ2 + "Abandon J2 - Victoire J1";
             } else if (!isP1playing) {
-                return nbreJeuJ1 + " : " + nbreJeuJ2 + "Abandon J1 - Victoire J2";
+                return nbreJeuJ1 + " - " + nbreJeuJ2 + "Abandon J1 - Victoire J2";
             } else {
-                return nbreJeuJ1 + " : " + nbreJeuJ2;
+                return nbreJeuJ1 + " - " + nbreJeuJ2;
             }
         }
     }
